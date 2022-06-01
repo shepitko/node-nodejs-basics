3fb07a50bd5626b2635c6c4e0d368b9f3b42f8e8
Shepitko <ya.shepitko@gmal.com>
2022-06-01 21:43:12 +0300

    feat: implement cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,34 @@
 export const parseArgs = () => {
-    // Write your code here 
+  const VARIABLE_PATTERN = /^--/;
+
+  const args = process.argv.slice(2).reduce((acc, arg, cur, arr) => {
+    if (arg.match(VARIABLE_PATTERN)) {
+      acc[arg.substring(2)] = true
+      acc['_lastkey'] = arg.substring(2)
+    } else {
+      if (arg.match(/^-[^-]/)) {
+        for (key of arg.substring(1).split('')) {
+          acc[key] = true
+          acc['_lastkey'] = key
+        }
+      } else {
+        if (acc['_lastkey']) {
+          acc[acc['_lastkey']] = arg
+          delete acc['_lastkey']
+        } else {
+          acc[arg] = true
+        }
+      }
+    }
+
+    if (cur == arr.length - 1)
+      delete acc['_lastkey']
+    return acc
+  }, {})
+
+
+  console.info(Object.entries(args).map(([key, val]) => `${key} is ${val}`).join(', '));
+
 };
+
+parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,27 @@
+#!/usr/bin/env node
+
+const VARIABLE_PATTERN = '^RSS'
+const DELIMITER = '='
+
 export const parseEnv = () => {
-    // Write your code here 
+
+    const args = process.argv.slice(2).reduce((acc, arg, cur, arr) => {
+        // Ignoring variables that do not start with RSS
+        if (arg.match(VARIABLE_PATTERN)) {
+            if (arg.match(DELIMITER)) {
+                const [key, value] = arg.split(DELIMITER);
+                acc.push({ key, value });
+            } else {
+                // Variable without value must be boolean
+                acc.push({ key: arg, value: true })
+            }
+        }
+
+        return acc
+    }, [])
+
+
+    console.info(args.map(({ key, value }) => `${key}=${value}`).join("; "));
 };
+
+parseEnv();
