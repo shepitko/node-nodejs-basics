9c03639653006c7bc89321fb784a562c89c52a67
Shepitko <ya.shepitko@gmal.com>
2022-06-02 23:04:50 +0300

    feat: implement zip/unzip

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,20 @@
+import zlib from 'zlib';
+import fs from 'fs';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const FILE = path.join(__dirname, 'files', 'fileToCompress.txt');
+const ARCHIVE = path.join(__dirname, 'files', 'archive.gz');
+
 export const decompress = async () => {
-    // Write your code here 
+    const input = fs.createReadStream(ARCHIVE);
+    const output = fs.createWriteStream(FILE, 'utf-8');
+    const gzip = zlib.createGunzip();
+
+    input.pipe(gzip).pipe(output);
 };
+
+await decompress()
