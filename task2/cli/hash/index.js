9d74227a96a5a147e3820b65b85e34064ba8ebef
Shepitko <ya.shepitko@gmal.com>
2022-06-12 23:28:39 +0300

    feat: implement compress/decompress

--- task2/cli/hash/index.js
+++ task2/cli/hash/index.js
@@ -3,13 +3,24 @@ import { readFileSync } from 'fs';
 import path from 'path';
 import { cwd } from 'process'
 
+import { EXECUTION_ERROR } from '../../enums/messages.js';
+
 export const handleHashActions = ({ args, output }) => {
-	const file = path.join(cwd(), args[0]);
+	try {
+		const file = path.join(cwd(), args[0]);
 
-	const fileBuffer = readFileSync(file);
-	const hash = createHash('sha256');
+		const fileBuffer = readFileSync(file);
+		const hash = createHash('sha256');
 
-	hash.update(fileBuffer);
+		hash.update(fileBuffer);
 
-	output.write(hash.digest('hex'));
+		output.write(hash.digest('hex'));
+	} catch (e) {
+		output.write(EXECUTION_ERROR);
+	}
 }
+
+// Test case:
+//
+// cd files
+// hash showHashThisFile.txt
--- task2/enums/messages.js
+++ task2/enums/messages.js
@@ -3,3 +3,5 @@ export const FAREWELL = '\nThank you for using File Manager, {userName}';
 export const CURRENT_DIRECTORY = '\nYou are currently in {path}\n';
 export const WRONG_COMMAND = '\nInvalid input\n';
 export const EXECUTION_ERROR = '\nOperation failed\n';
+export const COMPRESS_MESSAGE = '\nFile was compressed\n'
+export const DECOMPRESS_MESSAGE = '\nFile was decompressed\n'
